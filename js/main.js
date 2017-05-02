5462b57c0c0261b99a6f18d57ce817abd275b008
Paraprax <parkerjohnston28@gmail.com>
2017-05-01 20:58:25 -0400

    adding lines to switch body between light and dark classes

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 $ (document).ready(function () {
-  $('.status').text('Fire The Emergency Booster Rockets!');
+
 
   $('.switch').click(function () {
     var self = $(this);
     var switchedon = self.hasClass('on');
       if (switchedon) {
+         $('body').removeClass('light').addClass('dark');
          self.removeClass('on').addClass('off');
+         $('.status').text('Hey, who turned off the lights?!');
       } else {
+         $('body').removeClass('dark').addClass('light');
          self.removeClass('off').addClass('on');
+         $('.status').text("It's so bright in here!");
       }
   });
 });
