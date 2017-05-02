f4c4e9183ca41481bdc840464284e3b1e5a8cf1f
Paraprax <parkerjohnston28@gmail.com>
2017-05-01 20:44:18 -0400

    adding s switch click on and off functions

--- js/main.js
+++ js/main.js
@@ -1,2 +1,13 @@
+$ (document).ready(function () {
+  $('.status').text('Fire The Emergency Booster Rockets!');
 
-
+  $('.switch').click(function () {
+    var self = $(this);
+    var switchedon = self.hasClass('on');
+      if (switchedon) {
+         self.removeClass('on').addClass('off');
+      } else {
+         self.removeClass('off').addClass('on');
+      }
+  });
+});
