1b53f1a8a7d09996a42acaac942c25369ddc007e
Paraprax <parkerjohnston28@gmail.com>
2017-05-02 02:00:34 -0400

    added darkplate class for indigo colour on switchplate when dark

--- js/main.js
+++ js/main.js
@@ -5,10 +5,12 @@ $ (document).ready(function () {
     var self = $(this);
     var switchedon = self.hasClass('on');
       if (switchedon) {
+         $('div').removeClass('plate').addClass('darkplate');
          $('body').removeClass('light').addClass('dark');
          self.removeClass('on').addClass('off');
          $('.status').text('Hey, who turned off the lights?!');
       } else {
+         $('div').removeClass('darkplate').addClass('plate');
          $('body').removeClass('dark').addClass('light');
          self.removeClass('off').addClass('on');
          $('.status').text("It's so bright in here!");
